ab775a26b5bca915430b6df7c9996f3acb0def75
Andrew <andrew@Andrews-iMac.local>
2016-02-18 11:23:19 -0800

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'staringatlights:autoform-generic-error',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'Enables generic error handling in AutoForm.',
   // URL to the Git repository containing the source code for this package.
